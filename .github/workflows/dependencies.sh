0c8b892b31fc923039f002f2e0bd77ed38bf3e5f
Zabaniya001 <zabaniya101@gmail.com>
2023-02-12 00:56:39 +0100

    CI

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -1,5 +1,10 @@
 mkdir stocksoup
 cd stocksoup
 
-wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/memory.inc"
-wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/tf/hud_notify.inc"
+wget https://raw.githubusercontent.com/nosoop/stocksoup/master/math.inc
+wget https://raw.githubusercontent.com/nosoop/stocksoup/master/memory.inc
+
+mkdir tf
+cd tf
+
+wget https://raw.githubusercontent.com/nosoop/stocksoup/master/tf/hud_notify.inc
