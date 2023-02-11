74f6da249825aca67a4f50d1b879b8f3fa6fa3bb
Zabaniya001 <zabaniya101@gmail.com>
2023-02-11 04:53:34 +0100

    update workflows

---
+++ .github/workflows/dependencies.sh
@@ -0,0 +1,5 @@
+mkdir stocksoup
+cd stocksoup
+
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/memory.inc"
+wget "https://raw.githubusercontent.com/nosoop/stocksoup/master/tf/hud_notify.inc"
